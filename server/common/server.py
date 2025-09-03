8f5008d117d953807c4c20a3caa4ab7f1db6a992
Agustina Landi <aglandi@fi.uba.ar>
2025-09-03 13:21:34 -0300

    modifico logs

--- server/common/server.py
+++ server/common/server.py
@@ -168,7 +168,7 @@ class Server:
     def _handle_winners_request(self, client_sock, message):
         try:
             if not self._draw_done:
-                logging.warning("action: winners_request | result: rejected | reason: draw_not_ready")
+                logging.warning("action: winners_request | result: fail | reason: draw_not_ready")
                 write_all(client_sock, b"WINNERS|\n")
                 return
 
